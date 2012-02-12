221fd725f0b8e654183639089a4520616b27361a
Alexander Gudulin <alexandr.gudulin@gmail.com>
2012-02-12 21:26:37 +0400

    Added zd02_3 solution

--- ucque_ga.pas
+++ ucque_ga.pas
@@ -1,18 +1,18 @@
-// uique_ga.pas			12.02.2012 	Гудулин А.О.
+// ucque_ga.pas			12.02.2012 	Гудулин А.О.
 // Модуль, реализующий представление очереди ОБЪЕКТОМ TIQueue
 // с соответствующими операциями работы с очередью
 
-unit uique_ga;
+unit ucque_ga;
 
 interface
 	type
 		TIQueue = Object
 			len: integer;
-			val: array[1..10] of integer;
+			val: array[1..50] of char;
 			procedure init;
-			function push(n:integer): boolean;
-			function pop(var n:integer): boolean;
-			function top(var n:integer): boolean;
+			function push(c:char): boolean;
+			function pop(var c:char): boolean;
+			function top(var c:char): boolean;
 			function error_handler(count:integer): boolean;
 		end;
 
@@ -21,29 +21,29 @@ implementation
 		var
 			i: integer;
 		begin
-			for i:=1 to 10 do
-				val[i] := 0;
+			for i:=1 to 50 do
+				val[i] := 'a';
 			len := 0;
 		end;
 
-	function TIQueue.push(n:integer): boolean;
+	function TIQueue.push(c:char): boolean;
 		begin
 			
 			if error_handler(len+1) then begin
 				inc(len);
-				val[len] := n;
+				val[len] := c;
 				push := true;
 			end
 			else
 				push := false;
 		end;
 	
-	function TIQueue.pop(var n:integer): boolean;
+	function TIQueue.pop(var c:char): boolean;
 		var
 			i: integer;
 		begin
 			if error_handler(len) then begin
-				n := val[1];
+				c := val[1];
 				dec(len);
 				for i:=1 to len do
 					val[i] := val[i+1];
@@ -53,10 +53,10 @@ implementation
 				pop := false;
 		end;
 
-	function TIQueue.top(var n:integer): boolean;
+	function TIQueue.top(var c:char): boolean;
 		begin
 			if error_handler(len) then begin
-				n := val[1];
+				c := val[1];
 				top := true;
 			end
 			else
@@ -67,7 +67,7 @@ implementation
 		var
 			error: byte;
 		begin
-			if (count > 10) then
+			if (count > 50) then
 				error := 1
 			else if (count < 1) then
 				error := 2
---
+++ zd02_3_ga.pas
@@ -0,0 +1,62 @@
+// zd02_3_ga.pas			12.02.2012	Гудулин А.О.
+// Программа тестирования модуля uique_ga
+
+uses ucque_ga;
+var
+	queue: TIQueue;
+	in_str: string;
+	i: char;
+
+procedure show_help;
+	begin
+		writeln('Список доступных команд:');
+		writeln('push':8);
+		writeln('pop':7);
+		writeln('top':7);
+		writeln('help':8);
+		writeln('exit':8);
+	end;
+
+begin
+	queue.init;
+	writeln('*** zd02_3_ga.pas          12.02.2012  Гудулин А.О.');
+	writeln('*** Программа тестирования функций работы с очередью');
+	writeln('***                        push, pop, top');
+	in_str := '';
+	writeln;
+	show_help;
+	while(true) do begin
+		writeln('Введите команду:');
+		write('>>> ');
+		readln(in_str);
+		writeln;
+		case(in_str) of
+			'push': begin
+						writeln('** Введите новое значение (char):');
+						write('>>> ');
+						readln(i);
+						if queue.push(i) then
+							writeln('** Значение помещено в очередь');
+						writeln;
+					end;
+			'pop' : begin
+						if queue.pop(i) then begin
+							writeln('** Значение, извлеченное из очереди:');
+							writeln(i);
+						end;
+						writeln;
+					end;
+			'top' : begin
+						if queue.top(i) then begin
+							writeln('** Значение, извлеченное из очереди:');
+							writeln(i);
+						end;
+						writeln;
+					end;
+			'help': show_help;
+			'exit': exit;
+			else
+				show_help;
+		end;
+	end;
+end.
